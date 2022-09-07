c305eb96910cec5e4762913088e261c2ea25c50c
Jakob Lingel <jlingel68@gmail.com>
2022-09-07 23:56:09 +0200

    Add delete project button, style, fira code font

--- src/scripts/project.ts
+++ src/scripts/project.ts
@@ -4,6 +4,7 @@ export class Project {
 
   private todos: Todo[] = [];
   private title: string;
+  private id: number;
 
   constructor(title: string) {
     this.title = title;
@@ -18,7 +19,25 @@ export class Project {
     this.todos.splice(indexOfTodo, 1);
   }
 
+  static projectCounter = (function() {
+    let counter = 0;
+    function count() {
+      counter++;
+      return counter;
+    }
+    return count;
+  })();
+
+  static makeDefaultProjectName() {
+    return `Project ${this.projectCounter()}`;
+  }
+
   get Todos() {
     return this.todos;
   }
+
+  get Title() {
+    return this.title;
+  }
+
 }
--- src/scripts/projectView.ts
+++ src/scripts/projectView.ts
@@ -1,19 +1,46 @@
 import { Project } from "./project";
 
-let newProjectButton = document.createElement("button");
-newProjectButton.classList.add("projectAddButton");
-newProjectButton.innerHTML = "+";
+let projectTabArea = document.querySelector(".projectTabs");
 let content = document.querySelector("header>div");
+let addProjectButton = createAddProjectButton();
 
-let maincontent = document.querySelector("main");
-
-content.appendChild(newProjectButton);
-
-newProjectButton.addEventListener('click', () => {
-  let project = new Project("myProject");
-  let card = document.createElement("div");
-  card.classList.add('card');
-  maincontent.appendChild(card);
-
+addProjectButton.addEventListener('click', () => {
+  let tab = createProjectTab();
+  projectTabArea.appendChild(tab);
   console.log("woot");
 });
+
+function createProjectTabDeleteButton(tab: HTMLElement) {
+  let deleteProjectTabButton = document.createElement('button');
+  deleteProjectTabButton.classList.add("deleteProjectTabButton");
+  deleteProjectTabButton.addEventListener('click', () => deleteProjectTab(tab));
+  return deleteProjectTabButton;
+}
+
+content.appendChild(addProjectButton);
+
+function createAddProjectButton() {
+  let addProjectButton = document.createElement("button");
+  addProjectButton.classList.add("projectAddButton");
+  return addProjectButton;
+}
+
+function createProjectTab() {
+  let project = new Project(Project.makeDefaultProjectName());
+  let tab = document.createElement("div");
+  tab.classList.add('tab');
+  tab.innerHTML = project.Title;
+  tab.addEventListener('click', () => switchTo(tab));
+  tab.appendChild(createProjectTabDeleteButton(tab));
+  return tab;
+}
+
+function switchTo(tab: HTMLElement) {
+  Project.load(tab.dataset.indexNumber);
+}
+
+function deleteProjectTab(tab: HTMLElement) {
+  console.log("deleting");
+  projectTabArea.removeChild(tab);
+}
+
