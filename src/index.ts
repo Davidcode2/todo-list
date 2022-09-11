b49960cd60f153157be1611bc1532b356344a119
Jakob Lingel <jlingel68@gmail.com>
2022-09-11 16:23:33 +0200

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,11 @@ import './scripts/projectView';
 import { ProjectTabView } from "./scripts/projectView";
 import { ProjectManager } from './scripts/projectManager';
 import { TodoView } from './scripts/todoView';
+import { App } from './scripts/app';
+import { TodoManager } from './scripts/todoManager';
 
 let projectManager = new ProjectManager();
 new ProjectTabView(projectManager);
 new TodoView();
-
-let myProject = new Project();
-let myTodo = new Todo("mop floor");
-myProject.addTodo(myTodo);
-
-console.log(myProject.Todos);
 
 console.log("yay");
