57337c3a4c6ef3478e09ee1c54fe0904a7a39971
Jakob Lingel <jlingel68@gmail.com>
2022-09-11 13:19:44 +0200

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,6 @@ import { ProjectTabView } from "./scripts/projectView";
 import { ProjectManager } from './scripts/projectManager';
 import { TodoView } from './scripts/todoView';
 
-let main = document.querySelector("main");
-let content = document.createElement("div");
-content.id = "content";
-
-main.appendChild(content);
-content.innerHTML = "This is inside of content";
-
 let projectManager = new ProjectManager();
 new ProjectTabView(projectManager);
 new TodoView();
